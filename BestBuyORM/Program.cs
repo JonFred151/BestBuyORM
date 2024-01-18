d9ec15db922548adc71e3e0570ca2c9672bad546
Jon Frederick <jonFred151@gmail.com>
2024-01-18 14:46:25 -0600

    Complete exercise 2

--- BestBuyORM/Program.cs
+++ BestBuyORM/Program.cs
@@ -20,39 +20,23 @@ namespace BestBuyORM
             
             #endregion
 
+            IDbConnection connection = new MySqlConnection(connString);
+            var Prod = new DapperProductRepository(connection);
 
-            IDbConnection conn = new MySqlConnection(connString);
-            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
-
-            Console.WriteLine("Hello user, here are the current departments:");
-            Console.WriteLine("Please press enter . . .");
-            Console.ReadLine();
-            var depos = repo.GetAllDepartments();
-            printing(depos);
-
-            Console.WriteLine("Do you want to add a department???");
-            string userResponse = Console.ReadLine();
-
-            if(userResponse.ToLower() == "yes") 
+            Prod.CreateProduct("New Stuff", 20, 1);
+            var products = Prod.GetAllProducts();
+            foreach (var product in products)
             {
-                Console.WriteLine("What is the name of your new department?");
-                userResponse =Console.ReadLine();
-
-                repo.InsertDepartment(userResponse);
-                repo.GetAllDepartments();
-                printing(repo.GetAllDepartments());
+                Console.WriteLine($"{product.ProductID} {product.Name}");}
             }
-            Console.WriteLine("Have a great day!");
+
+
+
+           
 
 
         }
 
-        private static void printing(IEnumerable<Department> depos)
-        {
-            foreach (var depo in depos)
-            {
-                Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
-            }
-        }
+       
     }
 }
