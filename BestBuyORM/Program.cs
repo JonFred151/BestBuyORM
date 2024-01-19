c73e4b4a83c62e142176ea05837d89116080c8ec
Jon Frederick <jonFred151@gmail.com>
2024-01-18 20:20:02 -0600

    merging branched

--- BestBuyORM/Program.cs
+++ BestBuyORM/Program.cs
@@ -29,14 +29,52 @@ namespace BestBuyORM
             {
                 Console.WriteLine($"{product.ProductID} {product.Name}");}
             }
+        /* 
+         * 
+         * First exercise
+         * 
+         * 
+         * 
+         *    IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
 
+            Console.WriteLine("Hello user, here are the current departments:");
+            Console.WriteLine("Please press enter . . .");
+            Console.ReadLine();
+            var depos = repo.GetAllDepartments();
+            printing(depos);
+
+            Console.WriteLine("Do you want to add a department???");
+            string userResponse = Console.ReadLine();
+
+            if(userResponse.ToLower() == "yes") 
+            {
+                Console.WriteLine("What is the name of your new department?");
+                userResponse =Console.ReadLine();
+
+                repo.InsertDepartment(userResponse);
+                repo.GetAllDepartments();
+                printing(repo.GetAllDepartments());
+            }
+            Console.WriteLine("Have a great day!");
+
+
+        }
+
+        
 
 
            
 
 
         }
-
-       
+        private static void printing(IEnumerable<Department> depos)
+        {
+            foreach (var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
+            }
+        }
+       */
     }
 }
